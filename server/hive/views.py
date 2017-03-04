4f3870942d0b23d2054221bbc402b5bde581d0ec
Amy Shu <awshu1997@gmail.com>
2017-03-04 14:37:29 -0800

    Added password cracking

--- server/hive/views.py
+++ server/hive/views.py
@@ -2,26 +2,33 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.files.base import ContentFile
 
-import os
+from password import crack_password
 
-BASE_PATH = "files/"
+import os
+import getpass
+
+HCCAP_FILENAME = 'hashcat.hccap'
+HIVE_BLACKLIST = [4]
 
 def crack(request):
     if request.method == 'POST':
-        save_file(request.FILES['h_file'])
-        return HttpResponse("SAVED.")
+        f = request.FILES['h_file']
+        file_content = ContentFile(f.read())
+
+        home = os.path.expanduser("~")
+        full_filename = os.path.join(home, HCCAP_FILENAME)
+
+        # saves file
+        fout = open(full_filename, 'wb+')
+        for chunk in file_content.chunks():
+            fout.write(chunk)
+        fout.close()
+
+        password = crack_password(HCCAP_FILENAME, HIVE_BLACKLIST)
+
+        # deletes hccap file
+        os.system('rm ' + full_filename)
+
+        return HttpResponse(password)
     return HttpResponse("not post")
 
-def save_file(f):
-    try:
-        os.mkdir(BASE_PATH)
-    except:
-        pass
-
-    full_filename = os.path.join(BASE_PATH, f.name)
-    file_content = ContentFile(f.read())
-
-    fout = open(full_filename, 'wb+')
-    for chunk in file_content.chunks():
-        fout.write(chunk)
-    fout.close()
